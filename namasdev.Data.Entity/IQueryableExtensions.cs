48870ff2002995d0127f4666ee8712edc3c7e058
Matias Lopez <matutelopez@hotmail.com>
2025-04-28 12:13:21 -0300

    [main] (IQueryableExtensions) Agregar: IncludeMultiple, IncludeMultipleIf

--- namasdev.Data.Entity/IQueryableExtensions.cs
+++ namasdev.Data.Entity/IQueryableExtensions.cs
@@ -2,15 +2,61 @@
 using System.Data.Entity;
 using System.Linq.Expressions;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace namasdev.Data.Entity
 {
     public static class IQueryableExtensions
     {
-        public static IQueryable<T> IncludeIf<T, TProperty>(this IQueryable<T> query, Expression<Func<T, TProperty>> path, bool condition)
+        public static IQueryable<T> IncludeIf<T, TProperty>(this IQueryable<T> query, 
+            Expression<Func<T, TProperty>> path, bool condition)
         {
             return condition
                 ? query.Include(path)
+                : query;
+        }
+
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query,
+            IEnumerable<string> paths)
+        {
+            if (paths != null)
+            {
+                foreach (var p in paths)
+                {
+                    query = query.Include(p);
+                }
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query,
+            IEnumerable<Expression<Func<T, object>>> paths)
+        {
+            if (paths != null)
+            {
+                foreach (var p in paths)
+                {
+                    query = query.Include(p);
+                }
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> IncludeMultipleIf<T>(this IQueryable<T> query,
+            IEnumerable<string> paths, bool condition)
+        {
+            return condition
+                ? query.IncludeMultiple(paths)
+                : query;
+        }
+
+        public static IQueryable<T> IncludeMultipleIf<T>(this IQueryable<T> query,
+            IEnumerable<Expression<Func<T, object>>> paths, bool condition)
+        {
+            return condition
+                ? query.IncludeMultiple(paths)
                 : query;
         }
     }
