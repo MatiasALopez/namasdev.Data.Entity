54ca5d6ed85e1e634545ece0162d8e6e54f272ed
Matias Lopez <matutelopez@hotmail.com>
2025-05-29 17:04:13 -0300

    [main] Actualizar RepositorioSoloLectura para excluir entidades borradas

--- namasdev.Data.Entity/RepositorioSoloLectura.cs
+++ namasdev.Data.Entity/RepositorioSoloLectura.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 
 using namasdev.Core.Entity;
+using namasdev.Core.Reflection;
 using namasdev.Core.Types;
 
 namespace namasdev.Data.Entity
@@ -13,34 +14,62 @@ namespace namasdev.Data.Entity
         where TEntidad : class, IEntidad<TId>, new()
         where TId : IEquatable<TId>
     {
+        private readonly bool _entidadImplementaBorrado;
+
+        public RepositorioSoloLectura()
+        {
+            _entidadImplementaBorrado = ReflectionHelper.ClaseImplementaInterfaz<TEntidad, IEntidadBorrado>(); 
+        }
+
         public TEntidad Obtener(TId id)
+        {
+            return Obtener(id, incluirBorrados: false);
+        }
+
+        public TEntidad Obtener(TId id,
+            bool incluirBorrados)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
-                    .FirstOrDefault(e => e.Id.Equals(id));
+                var query = ctx.Set<TEntidad>()
+                    .Where(e => e.Id.Equals(id));
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query
+                    .FirstOrDefault();
             }
         }
 
-        public TEntidad Obtener(TId id, 
+        public TEntidad Obtener(TId id,
+            bool incluirBorrados = false,
             IEnumerable<string> cargarPropiedades = null)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
+                var query = ctx.Set<TEntidad>()
                     .IncludeMultiple(cargarPropiedades)
-                    .FirstOrDefault(e => e.Id.Equals(id));
+                    .Where(e => e.Id.Equals(id));
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query.FirstOrDefault();
             }
         }
 
-        public TEntidad Obtener(TId id, 
+        public TEntidad Obtener(TId id,
+            bool incluirBorrados = false,
             IEnumerable<Expression<Func<TEntidad, object>>> cargarPropiedades = null)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
+                var query = ctx.Set<TEntidad>()
                     .IncludeMultiple(cargarPropiedades)
-                    .FirstOrDefault(e => e.Id.Equals(id));
+                    .Where(e => e.Id.Equals(id));
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query.FirstOrDefault();
             }
         }
 
@@ -56,38 +85,81 @@ namespace namasdev.Data.Entity
         }
 
         public IEnumerable<TEntidad> ObtenerLista(
-            OrdenYPaginacionParametros op = null, 
-            IEnumerable<string> cargarPropiedades = null)
+            bool incluirBorrados,
+            OrdenYPaginacionParametros op = null)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
-                    .IncludeMultiple(cargarPropiedades)
+                var query = ctx.Set<TEntidad>().AsQueryable();
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query
                     .OrdenarYPaginar(op)
                     .ToArray();
             }
         }
 
         public IEnumerable<TEntidad> ObtenerLista(
-            OrdenYPaginacionParametros op = null,
-            IEnumerable<Expression<Func<TEntidad, object>>> cargarPropiedades = null)
+            bool incluirBorrados = false,
+            OrdenYPaginacionParametros op = null, 
+            IEnumerable<string> cargarPropiedades = null)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
-                    .IncludeMultiple(cargarPropiedades)
+                var query = ctx.Set<TEntidad>()
+                    .IncludeMultiple(cargarPropiedades);
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query
                     .OrdenarYPaginar(op)
                     .ToArray();
             }
         }
 
-        public bool ExistePorId(TId id)
+        public IEnumerable<TEntidad> ObtenerLista(
+            bool incluirBorrados = false,
+            OrdenYPaginacionParametros op = null,
+            IEnumerable<Expression<Func<TEntidad, object>>> cargarPropiedades = null)
         {
             using (var ctx = new TDbContext())
             {
-                return ctx.Set<TEntidad>()
-                    .Any(e => e.Id.Equals(id));
+                var query = ctx.Set<TEntidad>()
+                    .IncludeMultiple(cargarPropiedades);
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query
+                    .OrdenarYPaginar(op)
+                    .ToArray();
             }
+        }
+
+        public bool ExistePorId(TId id,
+            bool incluirBorrados = false)
+        {
+            using (var ctx = new TDbContext())
+            {
+                var query = ctx.Set<TEntidad>()
+                    .Where(e => e.Id.Equals(id));
+
+                query = AplicarFiltroBorrados(query, incluirBorrados);
+
+                return query.Any();
+            }
+        }
+
+        private IQueryable<TEntidad> AplicarFiltroBorrados(IQueryable<TEntidad> query, bool incluirBorrados)
+        {
+            if (!_entidadImplementaBorrado || incluirBorrados)
+            {
+                return query;
+            }
+
+            return ((IQueryable<IEntidadBorrado>)query)
+                .Where(e => !e.Borrado)
+                .Cast<TEntidad>();
         }
     }
 }
